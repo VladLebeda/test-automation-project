b85d81a8cc6a1070a389ab96adb8433dc31d88f7
VladLebeda <vladlebeda@gmail.com>
2021-05-03 18:28:11 +0300

    Test push to check github action CI.

--- tests/api/sample_test.py
+++ tests/api/sample_test.py
@@ -6,17 +6,19 @@ from tools.api_caller import ApiCaller
 class TestSample:
     def data_generator(self):  # Generating unique random data to ease future searches, also unique for each test run.
         random_key = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(6))
-        user_email = random_key + '@mail.ru'
+        user_email = random_key + '@gmail.com'
         user_name = random_key
         user_password = random_key[::-1]
         return user_email, user_name, user_password
 
+    @pytest.mark.api
     def test_registration(self):
         user_email, user_name, user_password = self.data_generator()
         user_creation = ApiCaller.create_user(user_email, user_name, user_password)
         user_creation_json = json.loads(user_creation.content)
         assert user_creation.status_code == 200 and user_name == user_creation_json['name']
 
+    @pytest.mark.api
     def test_email_only_registration(self):
         user_email, _, _ = self.data_generator()
         email_only = ApiCaller.create_user(user_email, '', '')
